91002bcec0e1ddd03a50ec9f9f867cf07d27af2c
brunobpcarvalho <bruno.bpcarvalho@gamil.com>
2020-08-09 15:02:08 -0300

    Removido o force: true.

--- models/Venda.js
+++ models/Venda.js
@@ -4,31 +4,31 @@ const Pessoa = require("./Pessoa");
 const Usuario = require("./Usuario");
 
 const Venda = db.define('vendas', {
-  dataVenda: {
-    type: Sequelize.DATEONLY
-  },
-  valorTotal: {
-    type: Sequelize.DECIMAL(10, 2)
-  },
-  desconto: {
-    type: Sequelize.DECIMAL(10, 2)
-  },
-  condicaoPagamento: {
-    type: Sequelize.STRING
-  },
-  parcelas: {
-    type: Sequelize.INTEGER
-  },
-  status: {
-    type: Sequelize.STRING
-  },
-  financeiro: {
-    type: Sequelize.STRING,
-    defaultValue: 'nao'
-  }
+    dataVenda: {
+        type: Sequelize.DATEONLY
+    },
+    valorTotal: {
+        type: Sequelize.DECIMAL(10, 2)
+    },
+    desconto: {
+        type: Sequelize.DECIMAL(10, 2)
+    },
+    condicaoPagamento: {
+        type: Sequelize.STRING
+    },
+    parcelas: {
+        type: Sequelize.INTEGER
+    },
+    status: {
+        type: Sequelize.STRING
+    },
+    financeiro: {
+        type: Sequelize.STRING,
+        defaultValue: 'nao'
+    }
 })
 
 Venda.belongsTo(Pessoa);
 Venda.belongsTo(Usuario);
-Venda.sync({force: true})
+Venda.sync()
 module.exports = Venda;
