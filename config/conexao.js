7daeb61ddbf013bc3fd1d0d00203c8c5ca0fa27d
Bruno Carvalho <38400685+brunobpcarvalho@users.noreply.github.com>
2021-06-20 17:40:33 -0300

    testew

--- config/conexao.js
+++ config/conexao.js
@@ -6,7 +6,7 @@ if (process.env.DATABASE_URL) {
 	conexao = new Sequelize(process.env.DATABASE_URL, {
 		dialect:  'postgres',
 		protocol: 'postgres',
-		dialectOptions: {"ssl": true},
+		dialectOptions: {"ssl": false},
         	logging: false
   })
 	console.log(conexao)
